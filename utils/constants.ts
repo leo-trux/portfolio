ebabfc2b395a62773f047eaf33139300c20054c0
Léo TRUX <truxleo@gmail.com>
2026-09-08 13:43:53 +0200

    up

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,10 +2,6 @@ const R2_URL: string | undefined = process.env.NEXT_PUBLIC_R2_URL
 
 export const getR2ImageUrl: (path: string) => string = (path: string) => {
     if (R2_URL == null) return ''
-    const base: string = R2_URL.startsWith('http://') || R2_URL.startsWith('https://')
-        ? R2_URL
-        : `https://${R2_URL}`
-    const cleanBase: string = base.endsWith('/') ? base.slice(0, -1) : base
     const cleanPath: string = path.startsWith('/') ? path.slice(1) : path
-    return `${cleanBase}/${cleanPath}`
+    return `${R2_URL}/${cleanPath}`
 }
